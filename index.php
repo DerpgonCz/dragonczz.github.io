0c10af621796313dd983042bbcbc9cfa10a71e09
DragonCzz <dragonczz@gmail.com>
2015-02-02 21:34:01 +0100

    Remove PHP and embed external site instead

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$id = $_GET['id'];
-$link = 'http://i.ytimg.com/vi/'.$id.'/'.(strlen(file_get_contents('http://i.ytimg.com/vi/'.$id.'/maxresdefault.jpg')) > 0 ? 'maxresdefault.jpg' : 'sddefault.jpg');
-echo '<img src="'.$link.'" /><br />';
-echo $link;
-?>
